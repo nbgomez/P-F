4cdb68e89cb350556262cb4e8b4e02b39ff2c9a0
Nick Gomez <ngomez@itnes.com>
2016-01-13 21:03:09 -0700

    added controller and partial view

---
+++ public/javascripts/inputCtrl.js
@@ -0,0 +1,8 @@
+(function() {
+  
+  var app=angular.module('myApp');
+  
+  app.controller( 'inputCtrl', ['$scope','$routeParams', function($scope,$routeParams){//,'$http', '$resource','NgTableParams','$filter', '$timeout',function($scope,$routeParams, $http, $resource, NgTableParams, $filter, $timeout){
+  
+  }]);
+})();
