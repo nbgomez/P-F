8703f309e30273676aee1d0c22141589d98a07db
Nick Gomez <ngomez@itnes.com>
2016-01-13 20:15:07 -0700

    initial commit

---
+++ public/javascripts/controllers.js
@@ -0,0 +1,14 @@
+(function(){
+
+	var myApp = angular.module( "myApp", ['ngRoute']);//'ngRoute', 'nvd3ChartDirectives','ngResource','ngTable','ngSanitize', 'ngCsv', 'ui.bootstrap','angular-loading-bar','nvd3', 'angularjs-datetime-picker', 'ui.bootstrap.datetimepicker' ]);
+	
+	myApp.config(['$routeProvider',function( $routeProvider ){
+    $routeProvider.
+    when('/', {
+      templateUrl:"/partials/input.html",
+      controller:"inputCtrl"
+    } ).
+    otherwise({
+      redirectTo: '/'
+    });
+})();
