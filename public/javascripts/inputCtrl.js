b1d99b74535d92b3cc2e0bdbe7508cb827fc055d
Nick Gomez <ngomez@itnes.com>
2016-01-17 21:06:33 -0700

    added client support to get prices.

--- public/javascripts/inputCtrl.js
+++ public/javascripts/inputCtrl.js
@@ -2,7 +2,18 @@
   
   var app=angular.module('myApp');
   
-  app.controller( 'inputCtrl', ['$scope','$routeParams', function($scope,$routeParams){//,'$http', '$resource','NgTableParams','$filter', '$timeout',function($scope,$routeParams, $http, $resource, NgTableParams, $filter, $timeout){
+  app.controller( 'inputCtrl', ['$scope','$routeParams','$http', function($scope,$routeParams, $http){//,'$http', '$resource','NgTableParams','$filter', '$timeout',function($scope,$routeParams, $http, $resource, NgTableParams, $filter, $timeout){
+  	$scope.symbol = "aapl";
+  	
+  	$scope.getPrices = function () {
+  		
+  		console.log( $scope.symbol );	
+  		$http.get( '/getPrices/:'+$scope.symbol ).success( function (response) {
+  			console.log( response );
+  		});
+  	}
+  	
+  	
   
   }]);
 })();
